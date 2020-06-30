8ac3f32bf934f6e13f61cfa4ee881f47126059cf
benmarcotte <55809994+benmarcotte@users.noreply.github.com>
2020-06-30 00:25:34 -0400

    Started project
    added v. basic start menu, cuppa scripts

---
+++ drinka2020/Assets/Scripts/GameHandler.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameHandler : MonoBehaviour
+{
+
+    [SerializeField] public Player[] players;
+    // Start is called before the first frame update
+
+    private void Awake()
+    {
+        int gameHandlerCount = FindObjectsOfType<GameHandler>().Length;
+        if(gameHandlerCount > 1)
+        {
+            Destroy(gameObject);
+        } 
+        else
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
